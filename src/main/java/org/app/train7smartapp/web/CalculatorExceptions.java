e30ee4dfd0543c21d1dd64a4277b1b0e2af1bdd4
toni1423 <todorow.1423@gmail.com>
2025-04-10 20:53:28 +0300

    Update CalculatorExceptions.java

--- src/main/java/org/app/train7smartapp/web/CalculatorExceptions.java
+++ src/main/java/org/app/train7smartapp/web/CalculatorExceptions.java
@@ -1,4 +1,4 @@
-//package org.app.train7smartapp.web;
+package org.app.train7smartapp.web;//package org.app.train7smartapp.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
 
-//public class CalculatorExceptions {
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ModelAndView handleCalculatorFeignCallException() {
-//        ModelAndView mav = new ModelAndView("error-not-found");
-//        mav.addObject("errorTitle", "Oops! We couldn't find that.");
-//        mav.addObject("errorStatus", "404 MethodArgumentTypeMismatchException.");
-//        return mav;
-//    }
-//}
+public class CalculatorExceptions {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ModelAndView handleCalculatorFeignCallException() {
+        ModelAndView mav = new ModelAndView("error-not-found");
+        mav.addObject("errorTitle", "Oops! We couldn't find that.");
+        mav.addObject("errorStatus", "404 MethodArgumentTypeMismatchException.");
+        return mav;
+    }
+}
